85aa5a680bad909f4e4dfa71a40a8e468c52fd8e
Josh Waller <mdx.programs@gmail.com>
2020-03-24 10:27:20 -0400

    adding more haskell support for development

--- settings/hs.el
+++ settings/hs.el
@@ -13,6 +13,19 @@
 (use-package hindent
   :ensure t)
 
+(use-package ghc
+  :ensure t
+  :init
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil t)
+  (add-hook 'haskell-mode-hook (lambda () (ghc-init))))
+
+(use-package company-ghc
+  :ensure t
+  :init
+  (add-to-list 'company-backends 'company-ghc)
+  (custom-set-variables '(company-ghc-show-info t)))
+
 (use-package shakespeare-mode
   :ensure t)
 
